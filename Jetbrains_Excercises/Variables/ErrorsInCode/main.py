2ebc8c79dac07e0d1e2b8929c5a83b1e546f7bbb
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2022-08-15 23:42:54 +0200

    added simple temperature converter

---
+++ Jetbrains_Excercises/Variables/ErrorsInCode/main.py
@@ -0,0 +1,9 @@
+#
+# Errors in the code
+# Imagine your friend has created a code for checking the type of variable (ost = 'Happy'),
+# but you have found some errors in it. Help him: find two or three errors, correct the code
+# and print the type of the variable.
+#
+
+ost = 'Happy'
+print(type(ost))
---
+++ Practice/Temperature_Converter/temperature_converter.py
@@ -0,0 +1,40 @@
+value = 0
+choice = 0
+
+
+def take_input():
+    while True:
+        try:
+            global value
+            value = float(input("Enter the value to convert: "))
+            break
+        except(RuntimeError, TypeError, NameError, ValueError):
+            print("Something went wrong.")
+
+
+def make_convert_choice():
+    text = """Enter the number 1, if you want to convert to Celsius
+or number 2 if you want to convert to Fahrenheit. """
+    global choice
+    choice = int(input(text))
+    match choice:
+        case 1:
+            print("The temperature is %.2f Celsius." % convert_to_celsius(value))
+        case 2:
+            print("The temperature is %.2f Fahrenheit." % convert_to_fahrenheit(value))
+        case _:
+            print("No valid number given.")
+
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5.0 / 9.0
+
+
+def convert_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5.0 + 32
+
+
+if __name__ == "__main__":
+    print("Welcome to Celsius-Fahrenheit Converter")
+    take_input()
+    make_convert_choice()
