f5e6979215da3d029df660fefdb6f4a5efddc4a4
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2022-10-18 00:54:30 +0200

    Added task

---
+++ Jetbrains_Excercises/Multiline/a_week.py
@@ -0,0 +1,26 @@
+# A week
+# Write the code that will print all days of the week on separate lines with an empty line between them. Make sure that each day starts with a capital letter. Start it with Sunday.
+#
+#
+# Sample Input:
+#
+# Sample Output:
+# Sunday
+#
+# Monday
+#
+# Tuesday
+#
+# Wednesday
+#
+# Thursday
+#
+# Friday
+#
+# Saturday
+
+
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for day in week:
+    print(day)
+    print("")
